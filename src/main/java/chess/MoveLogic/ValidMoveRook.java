6e2b0a90ba80fc54dac20fae36dcd87c4efdfd84
jens <jens@DESKTOP-4KCHRC0>
2022-03-25 08:29:55 +0100

    Forgot that I was on the main, Interface and beginning of valid move logic for Rook

---
+++ src/main/java/chess/MoveLogic/ValidMoveRook.java
@@ -0,0 +1,24 @@
+package chess.MoveLogic;
+
+import java.util.List;
+
+import chess.ChessBoard;
+import chess.Move;
+
+public class ValidMoveRook implements IValidMove
+{
+    ChessBoard chessBoard;
+
+    public ValidMoveRook(ChessBoard chessBoard)
+    {
+        this.chessBoard = chessBoard;
+    }
+
+    @Override
+    public List<Move> getValidMoves() 
+    {
+        
+        return null;
+    }
+    
+}
