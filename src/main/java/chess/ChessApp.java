497df7c56be74ebdc3a8ab79fd663ad575de74cd
didriknaess <didriknaess@hotmail.com>
2022-04-02 18:53:51 +0200

    restructured for FXML reading

---
+++ src/main/java/chess/ChessApp.java
@@ -0,0 +1,19 @@
+package chess;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class ChessApp extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Chess");
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("ChessGUI.fxml"))));
+        primaryStage.show();
+    }
+}
