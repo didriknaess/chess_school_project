177dcd130cfa478904130acdd08bdb83862b499c
jens <jens@DESKTOP-4KCHRC0>
2022-04-22 17:39:39 +0200

    Bruh ImageIO :(

--- src/main/java/chess/IO/ImageIO.java
+++ src/main/java/chess/IO/ImageIO.java
@@ -10,6 +10,41 @@ public class ImageIO {
     public ImageIO() {
     }
     public Image getImage(Piece p) {
+        switch(p.getColor()) {
+            case BLACK:
+                switch(p.getType()) {
+                    case PAWN:
+                        return new Image(getClass().getResourceAsStream("pics/BPawn.png"));
+                    case ROOK:
+                        return new Image(getClass().getResourceAsStream("pics/BRook.png"));
+                    case BISHOP:
+                        return new Image(getClass().getResourceAsStream("pics/BBishop.png"));
+                    case KING:
+                        return new Image(getClass().getResourceAsStream("pics/BKing.png"));
+                    case KNIGHT:
+                        return new Image(getClass().getResourceAsStream("pics/BKnight.png"));
+                    case QUEEN:
+                        return new Image(getClass().getResourceAsStream("pics/BQueen.png"));
+            }
+            case WHITE:
+                switch(p.getType()) {
+                    case PAWN:
+                        return new Image(getClass().getResourceAsStream("pics/WPawn.png"));
+                    case ROOK:
+                        return new Image(getClass().getResourceAsStream("pics/WRook.png"));
+                    case BISHOP:
+                        return new Image(getClass().getResourceAsStream("pics/WBishop.png"));
+                    case KING:
+                        return new Image(getClass().getResourceAsStream("pics/WKing.png"));
+                    case KNIGHT:
+                        return new Image(getClass().getResourceAsStream("pics/WKnight.png"));
+                    case QUEEN:
+                        return new Image(getClass().getResourceAsStream("pics/WQueen.png"));
+            }
+        }
+        return null;
+    }
+    public Image getImageTest(Piece p) {
         String imageFilePath = "";
         switch(p.getColor()) 
         {   
@@ -55,7 +90,9 @@ public class ImageIO {
                         imageFilePath = "WQueen.png";
                         break;
             }
-            System.out.println(new Image("/C:/src/objekt_project/target/classes/pics/WQueen.png"));
+            // URL url = getClass().getResource("/drawIcon.png");
+            // Image image = ImageIO.read(url);    
+            // return image;
         }
         return null;
     }
